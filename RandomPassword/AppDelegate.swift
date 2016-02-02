c4a4941fd8d857028ad8f95ba4afac6bc2c5547b
Drew Hagen <drew.a.hagen@gmail.com>
2016-02-02 16:04:01 -0600

    Initial Commit

---
+++ RandomPassword/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by Drew Hagen on 2/2/16.
+//  Copyright © 2016 Drew Hagen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
